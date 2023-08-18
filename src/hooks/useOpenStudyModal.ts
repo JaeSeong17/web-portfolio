3a0eddb1f859d00375b5aa4fd9de4d798306d136
AnJaeSeong <dktmzh6@gmail.com>
2023-08-18 23:21:32 +0900

    FEAT: update hooks of openStudyModal

--- src/hooks/useOpenStudyModal.ts
+++ src/hooks/useOpenStudyModal.ts
@@ -1,15 +1,15 @@
 import { create } from 'zustand'; // 상태관리 라이브러리 (Redux 계열)
 
-interface useRentModalProps {
+interface useOpenStudyModalProps {
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
 }
 
-const useRentModal = create<useRentModalProps>((set) => ({
+const useRegisterModal = create<useOpenStudyModalProps>((set) => ({
   isOpen: false,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
 }));
 
-export default useRentModal;
+export default useRegisterModal;
